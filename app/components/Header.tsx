949c48bdf720e5ca04befbc5842bc9b255cbc212
lizzyjoo <jejviolinist@gmail.com>
2025-11-28 20:14:20 -0500

    header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-amber-50 text-white p-4 mb-6">
+      <h1 className="text-2xl font-bold">Open Roles</h1>
+    </header>
+  );
+}
