509b23422818e94eee405f2e15919efcc9072ee3
lizzyjoo <jejviolinist@gmail.com>
2025-11-28 23:01:05 -0500

    fetch gist json

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import JobBoard from "./components/JobBoard";
 export default async function Page() {
   // response object
   const result = await fetch(
-    "https://gist.githubusercontent.com/lizzyjoo/350effdb1cfab92af924f0017301de10/raw/17bb47ac3bd0fe26006cb01052e4b0e5426b7274/jobs.json"
+    "https://gist.githubusercontent.com/lizzyjoo/350effdb1cfab92af924f0017301de10/raw/ec31a74f78a7515a9ff1400fb04441cf88d9227e/jobs.json"
   );
   const jobs = await result.json();
 
